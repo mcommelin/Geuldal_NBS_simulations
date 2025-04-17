c44d26f210099bf20b4985940da94c608427efe3
mcommelin <meindert.commelin@wur.nl>
2025-04-17 16:02:08 +0200

    working function to make rainfall input from radar

--- sources/r_scripts/create_rainfall_discharge_lisem.R
+++ sources/r_scripts/create_rainfall_discharge_lisem.R
@@ -9,6 +9,8 @@ library(hms)
 library(gdalUtilities)
 library(raster)
 library(sf)
+library(foreach)
+library(doParallel)
 library(tidyverse)
 
 # load pcraster functions
@@ -27,6 +29,9 @@ add_suffix <- function(strings) {
   })
 }
 
+n_cores <- detectCores() # number of cores
+registerDoParallel(cores = n_cores - 2) # register the cluster
+
 #1. make LISEM input precipitation maps ---------------------------------------
 
 # function that
@@ -35,16 +40,39 @@ add_suffix <- function(strings) {
 # selects catchment
 # and then makes the precipitation input
 
+# settings on resolution, event and catchment
+resolution = 20 # 5 or 20
+event_num = 2 # 1, 2, or 3
+catch_num = 12 # 1 = Geul 2 - 12 = subcatchments see points table
+
+
+rain_maps_lisem <- function(
+    event_num = event_num,
+  resolution = resolution,
+  catch_num = catch_num
+) {
+  
 #load only the events that will be used for calibration events
 events <- read_csv("sources/selected_events.csv") %>%
   filter(use == "cal") %>%
   mutate(ts_start = ymd_hms(event_start),
          ts_end = ymd_hms(event_end))
 
+#read catchment information
+points <- read_csv("LISEM_data/tables/outpoints_description.csv")
+
+# set options to enough digits for accuracy extent
+options(digits = 10)
+
+# get the catchment name
+catch_name <- points %>%
+  filter(point == catch_num) %>%
+  filter(cell_size == resolution) %>%
+  select(subcatch_name)
 
 
-
-for (k in seq_along(events$event_start)) {  
+ k = event_num
+  
   event_start <- events$ts_start[k]
   event_end <- events$ts_end[k]
   hours <- seq(event_start, event_end, by = "hours")
@@ -60,55 +88,87 @@ for (k in seq_along(events$event_start)) {
   map_names <- map_names %>%
     paste0("NSL_", ., ".ASC")
   
+  # get the correct directories
+  if (catch_num == 1) {
+    # Geul
+    main_dir <- paste0("LISEM_data/Geul_", resolution, "m/")
+  } else {
+    # subcatchments
+    main_dir <- paste0("LISEM_data/subcatchments/", catch_name, "_", resolution, "m/")
+  }
+  
+  # event name
+  ev_name <- as.character(event_start) %>%
+    str_remove_all("-") %>%
+    str_extract("^([0-9]{8})") %>%
+    paste0("rain_", .)
+  
   # general settings
   srs = "EPSG:28992"
-  cell_size <- c(5, 20)
   method = "near"
   
-  # give the extent of the masks for both resolutions
-  # set options to enough digits for accuracy extent
-  options(digits = 10)
-  extent <- matrix(
-    byrow = TRUE,
-    nrow = 2,
-    c(
-      178004.155600000,295805.049200000,207444.155600000,325845.049200000,
-      178008.314700000,295800.098400000,207448.314700000,325840.098400000
-    )
+
+  # get the extent of the mask.map
+  map2asc(
+    map_in = paste0(main_dir, "maps/mask.map"),
+    map_out = paste0(main_dir, "mask.asc")
   )
   
-  for (j in seq_along(cell_size)) {
-    sub_dir <- paste0("Geul_", cell_size[j], "m/")
-    
-    # make folder for event
-    if (!dir.exists(paste0("LISEM_data/", sub_dir, "rain/", ev_name))) {
-      dir.create(paste0("LISEM_data/", sub_dir, "rain/", ev_name))
+  # calculate extent from ascii header
+  # read the header
+  header <- readLines(paste0(main_dir, "mask.asc"), n = 5)
+  # extract only digits or '.' from the strings in header
+  header <- gsub("[^0-9.]", "", header)
+  
+  # convert to numeric
+  header <- as.numeric(header, digits = 10)
+  
+  # get the extent
+  extent <- c(
+    header[3], # xllcorner
+    header[4], # yllcorner
+    header[3] + header[1] * header[5], # xurcorner
+    header[4] + header[2] * header[5]  # yurcorner
+  )
+  
+  #remove the mask.asc
+  file.remove(paste0(main_dir, "mask.asc"))
+  
+    # make folder for rainfall event
+    if (!dir.exists(paste0(main_dir, "rain/", ev_name))) {
+      dir.create(paste0(main_dir, "rain/", ev_name), recursive = T)
     }
     
-    for (i in seq_along(map_names)) {
+  # parallel loop to make the rainfall maps
+    foreach(i = seq_along(map_names)) %dopar% {
+      
+     tempfile <- paste0("data/tmp_", Sys.getpid(), ".asc")
+       
       # reproject raster with gdal warp
       gdalwarp(
         srcfile = paste0("data/raw_data/neerslag/KNMI_radar_1uur/", map_names[i]),
-        dstfile = "data/tmp_rain.asc",
+        dstfile = tempfile,
         s_srs = srs,
         t_srs = srs,
         te_srs = srs,
-        tr = rep(cell_size[j], 2),
+        tr = rep(resolution, 2),
         of = "AAIgrid",
-        te = extent[j, ],
+        te = extent,
         r = method,
         dryrun = F,
         overwrite = T
       )
       # convert to pcraster format
-      asc2map(clone = paste0("LISEM_data/", sub_dir, "maps/mask.map"),
-              map_in = "data/tmp_rain.asc",
-              map_out = paste0("LISEM_data/", sub_dir, "rain/", ev_name, "/", rain_maps[i]),
+      asc2map(clone = paste0(main_dir, "maps/mask.map"),
+              map_in = tempfile,
+              map_out = paste0(main_dir, "rain/", ev_name, "/", rain_maps[i]),
               options = "-S")
+      # remove tmp rainfall file
+      file.remove(tempfile)
+      file.remove(paste0("data/tmp_", Sys.getpid(), ".prj"))
     }
-    # remove tmp rainfall file
-    file.remove("data/tmp_rain.asc")
-    file.remove("data/tmp_rain.prj")
+
+    
     
     # make corresponding rainfall file
     t <- tibble(ts = hours,
@@ -118,7 +178,7 @@ for (k in seq_along(events$event_start)) {
                              side = "left", pad = "0"),
              t_str = paste0("001:", t_str, " ", maps))
     
-    rain_file <- paste0("LISEM_data/", sub_dir, "rain/", ev_name, "/rain.txt")
+    rain_file <- paste0(main_dir, "rain/", ev_name, "/rain.txt")
     
     # write the header
     writeLines(paste0("# KNMI radar, resampled with GDAL warp, method = ", 
@@ -128,7 +188,7 @@ for (k in seq_along(events$event_start)) {
     write(t$t_str, file = rain_file, append = T)
   }
   
-}
+
 
 
 
