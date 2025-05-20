40af50febcaa24f758d3f63cff09f0bff494a5e4
mcommelin <meindert.commelin@wur.nl>
2025-05-20 09:18:35 +0200

    Start with code to calculate Q form crosssections etc

---
+++ sources/r_scripts/Q_measurements_Geuldal.R
@@ -0,0 +1,12 @@
+#' Calculations of discharge based on cross-sections etc.
+#' Based on the book 'Hydrometry' by Boiten.
+#' 
+#' Velocity measurement based on 1 point method (p82).
+#' Assuming that the waterdepth at .6 of the depth is about 
+#' the mean.
+#' 
+#' The following calculation of the discharge is done with the
+#' mid section method (p85). Since the cross section bins are not equally spaced.
+#'
+#'The flow speed is calculated from the equations provided with the flow meter
+#'
