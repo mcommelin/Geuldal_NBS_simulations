bd661156e26147a0f3b9aa125a29f6f389afe37a
mcommelin <meindert.commelin@wur.nl>
2025-05-20 09:07:56 +0200

    //

--- sources/r_scripts/discharge_rain_selection.R
+++ sources/r_scripts/discharge_rain_selection.R
@@ -41,7 +41,7 @@ q_file_dir = "data/raw_data/waterhoogte_metingen/metingen"
 q_files <- dir(q_file_dir, recursive = TRUE, pattern = ".csv$")
 
 # load file with station names and characteristics
-station_names <- read_csv("data/raw_data/waterhoogte_metingen/naam_meetpunten_Geuldal.csv")
+station_names <- read_csv("sources/naam_meetpunten_Geuldal.csv")
 # add long name for each station
 station_names <- station_names %>%
   mutate(name_long = paste0(naam, "_", ondertitel)) %>%
@@ -78,6 +78,8 @@ q_all <- q_all %>%
   mutate(wh = if_else(wh > 4000, wh / 100, wh)) %>%
   mutate(wh = if_else(wh > 400, wh / 10, wh))
 
+
+
 # filter in the waterhieght datasets to find:
 # 1. the events with high discharges at the outlet of the Geul in Meersen
 b <- q_all %>%
@@ -101,6 +103,20 @@ ggplot(b) +
   geom_point(aes(x = timestamp, y = wh))
 
 # the events + duration are added to 'sources/selected_events.csv'
+
+
+wh_ev23 <- q_all %>%
+  filter(timestamp > events$ts_start[1] &
+           timestamp < events$ts_end[1]) %>%
+  group_by(code) %>%
+  mutate(wh = wh - min(wh, na.rm = T),
+         whmax = max(wh, na.rm = T) - min(wh, na.rm = T)) %>%
+  #filter(type != "buffer") %>%
+  filter(naam == "Watervalderbeek")
+
+ggplot(wh_ev23) +
+  geom_line(aes(x = timestamp, y = wh, color = name_long)) + 
+  theme_classic()
 
 # 2 Visualize precipitation and discharge during events ------------------------
 
@@ -224,4 +240,61 @@ for (k in seq_along(events$event_start)) {
 event_summary <- bind_cols(events, event_summary)
 write_csv(event_summary,
           "data/processed_data/stats_selected_events.csv")
+
+# 3. Load high res Q and H data for selected events --------------------------
+
+# load the available Q data
+# make file list
+q_file_dir = "data/raw_data/debiet_ruwe_data"
+q_files <- dir(q_file_dir, recursive = TRUE, pattern = ".csv$")
+
+# load file with station names and characteristics
+station_names <- read_csv("sources/naam_meetpunten_Geuldal.csv")
+# add long name for each station
+station_names <- station_names %>%
+  mutate(name_long = paste0(naam, "_", ondertitel)) %>%
+  mutate(name_long = str_replace_all(name_long, " ", "_"))
+
+# load all files
+q_list <- vector("list", length = length(q_files))
+
+for (i in seq_along(q_files)) {
+  col_names_a <- names(read_delim(paste0(q_file_dir, "/", q_files[i]), delim = ";"))
+  a <- read_csv2(
+    paste0(q_file_dir, "/", q_files[i]),
+    col_names = col_names_a,
+    skip = 2
+  ) %>%
+    rename(timestamp = 'GMT+1') 
+  q_list[[i]] <- a
+}
+qall <- bind_rows(q_list) %>%
+  pivot_longer(
+    cols = (-timestamp),
+    names_to = "code",
+    values_to = "Q"
+  ) %>%
+  mutate(Q = if_else(Q < 0, NaN, Q)) %>%
+  filter(!is.na(Q)) %>%
+  filter(timestamp > "2020-01-01") %>%
+  left_join(station_names, by = "code")
+
+# load selected events  
+events <- read_csv("sources/selected_events.csv") %>%
+  mutate(ts_start = ymd_hms(event_start),
+         ts_end = ymd_hms(event_end)) %>%
+  filter(use != "none")
+
+qevent <- vector("list", length = nrow(events))
+
+for (k in seq_along(events$event_start)) {
+  qevent[[k]] <- qall %>%
+    filter(timestamp > events$ts_start[k] &
+             timestamp < events$ts_end[k])
+  
+  ggplot(qevent[[k]]) +
+    geom_line(aes(x = timestamp, y = Q, color = name_long)) +
+    theme_classic()
+}
+
 
