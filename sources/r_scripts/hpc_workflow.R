edfb2a91e8ef9329c0c53454c709cca75f561035
mcommelin <meindert.commelin@wur.nl>
2026-03-16 14:51:40 +0100

    add ini file for hpc execution

--- sources/r_scripts/hpc_workflow.R
+++ sources/r_scripts/hpc_workflow.R
@@ -1,13 +1,11 @@
 # read the arguments from the command line
 # run with:
 # Rscript --vanilla ./hpc_workflow.R args1 args2 args3 args4
+# example: Rscript --vanilla ./sources/r_scripts/hpc_workflow.R example.ini
 
 args = commandArgs(TRUE)
 
-prepare_data <- args[1]
-make_db <- args[2]
-make_hpc_run <- args[3]
-ini_file <- args[4]
+ini_file <- args[1]
 
 # Read ini file ----------------------------------------------------------------
 
@@ -39,3 +37,55 @@ ldd_subcatch(force_ldd = FALSE, res = c(5, 10, 20))
 }
 
 # Make base databases ----------------------------------------------------------
+
+
+#load the csv file to identify all sub catch numbers
+hpc_ids <- read_csv("sources/setup/hpc/subcatch_id_link.csv")
+
+# produce all subcatchments base maps
+#subnums <- hpc_ids$LISEM_ID
+
+# or use a subset (now Belgian part of the Gulp)
+subnums <- c(120, 125, 128, 130, 131, 140)
+
+# cut all the subcatchments from the Geul
+source("sources/r_scripts/create_subcatch_db.R")
+for (i in seq_along(subnums)) {
+  base_maps_subcatchment(cell_size = 10, sub_catch_number = subnums[i],
+                         run_type = "base", do_hpc = TRUE)
+}
+
+
+# make lisem runs ------------------------------------------------
+
+# make the swatre base file to produce inputs for the model runs
+# when a NBS is added, or settings are changed, repeat this step!
+# update landuse table, this works for all NBS solutions.
+source("sources/r_scripts/prepare_landuse_table.R")
+landuse_table_nbs()
+
+# make a new swatre file, this works for all NBS solutions.
+source("sources/r_scripts/swatre_input.R")
+swatre_file <- "swatre_NBS.csv"
+soil_landuse_to_swatre(file = "sources/setup/swatre/UBC_texture.csv",
+                       swatre_out = paste0("sources/setup/calibration/", swatre_file),
+                       do_NBS = TRUE
+)
+
+
+
+# make the actual run databases for the hpc
+# choices are:
+# include NBS: set a number, 0 = no nbs
+# whole Geul or subset?
+source("sources/r_scripts/create_hpc_run.R")
+create_hpc_run(subset = subnums,
+               swatre_file = swatre_file,
+               NBS_num = 0,    
+               resolution = 10,
+               dir_name = "",
+               run_type = "base",
+               do_runfile = TRUE,
+               cpu_cores = 6)
+
+
