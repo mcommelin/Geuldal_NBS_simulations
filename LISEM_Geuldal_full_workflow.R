23816db3ff5e7759b06af353666481c9e5065618
vjetten <v.jetten@kpnmail.nl>
2026-05-18 12:59:48 +0200

    // calibration

--- LISEM_Geuldal_full_workflow.R
+++ LISEM_Geuldal_full_workflow.R
@@ -150,7 +150,7 @@ for (i in seq_along(points_id)) {
 }
 
 # you can also run for one specific subcatchment e.g.
-base_maps_subcatchment(cell_size = 10, sub_catch_number = 54, run_type = "cal", calc_ldd = F)
+base_maps_subcatchment(cell_size = 20, sub_catch_number = 1, run_type = "cal", calc_ldd = F)
 
 # this databases can be used to create a LISEM run. Choices in settings or
 # calibration values can be set after this stage.
@@ -199,7 +199,7 @@ for (i in seq_along(points_id)) {
 
 # you can also run for one specific subcatchment e.g.
 create_lisem_run(resolution = 20, catch_num = 1, swatre_file = swatre_file, run_type = "cal", do_runfile = F)
-create_lisem_run(resolution = 10, catch_num = 18, swatre_file = swatre_file, run_type = "cal", do_runfile = F)
+create_lisem_run(resolution = 10, catch_num = 4, swatre_file = swatre_file, run_type = "cal", do_runfile = F)
 
 ## 2.5 Calibration settings and figures ----------------------------------------
 
